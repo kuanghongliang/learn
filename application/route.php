e6bfba7b44d2c653c6ab253fbca351e3eccade27
kuanghongliang <hongliang379@126.com>
2016-10-28 10:48:56 +0800

    路由定义

--- application/route.php
+++ application/route.php
@@ -8,7 +8,19 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
-
+use think\Route;
+// 注册路由到home模块的user控制器的reg操作
+//Route::rule('user/reg','home/User/reg');
+//Route::rule('user/login','home/User/login');
+// 注册路由到home模块的user控制器的reg操作,并且为get请求，不指定默认为任何请求
+//Route::rule('user/reg','home/User/reg','GET');
+//支持get post请求
+//Route::rule('user/login','home/User/login','GET|POST');
+//简化写法
+//Route::get('user/reg','home/User/reg');
+//批量注册,注册多个路由规则后，系统会依次遍历注册过的满足请求类型的路由规则，一旦匹配到正确的路由规则后则开始调用控制器的操作方法，后续规则就不再检测
+//Route::rule(['user/reg' => 'home/User/reg','user/login' =>'home/User/login']);
+//Route::post(['user/reg' => 'home/User/reg','user/login' =>'home/User/login']);
 return [
     '__pattern__' => [
         'name' => '\w+',
